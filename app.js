252dcd60603fbc73f5037b4a057df8b96d380bc2
chriszc97 <czeas1997@gmail.com>
2021-11-20 14:06:21 -0500

    grid

--- app.js
+++ app.js
@@ -1,83 +1,66 @@
 const mySnake = document.querySelector('.snake');
+let dot = document.createElement('div');
+document.querySelector('.gameGrid').appendChild(dot);
+dot.style.backgroundColor = 'rgb(70,108,90)';
+dot.style.borderRadius = '50%';
+
+// let eat = ()=>{
+//     if()
+// }
 let position = 10;
+let interval = null;
 
 window.addEventListener('load', () => {
   mySnake.style.position = 'relative';
   mySnake.style.left = 0;
   mySnake.style.top = 0;
+  dot.style.left = 0;
+  dot.style.top = 0;
 });
-let snakeMove = () => {
-  document.addEventListener('keydown', (e) => {
-    setInterval(() => {
-      switch (e.key) {
-        case 'ArrowLeft':
-          mySnake.style.left = parseInt(mySnake.style.left) - position + 'px';
-          if (mySnake.style.left === '-200px') {
-            mySnake.style.left = parseInt(mySnake.style.left) + position + 'px';
-            console.log('left limit');
-          }
-          break;
-
-        case 'ArrowRight':
-          mySnake.style.left = parseInt(mySnake.style.left) + position + 'px';
-          if (mySnake.style.left === '200px') {
-            mySnake.style.left = parseInt(mySnake.style.left) - position + 'px';
-            console.log('right limit');
-          }
-          break;
-        case 'ArrowUp':
-          mySnake.style.top = parseInt(mySnake.style.top) - position + 'px';
-          if (mySnake.style.top === '-200px') {
-            mySnake.style.top = parseInt(mySnake.style.top) + position + 'px';
-            console.log('up limit');
-          }
-          break;
-        case 'ArrowDown':
-          mySnake.style.top = parseInt(mySnake.style.top) + position + 'px';
-          if (mySnake.style.top === '200px') {
-            mySnake.style.top = parseInt(mySnake.style.top) - position + 'px';
-            console.log('down limit');
-          }
-          break;
-      }
-    }, 1000);
-  });
-};
-
-// snakeMove();
-// };
-setInterval(snakeMove(), 1000);
-
-// let snakeLeft = () => {
-//   document.addEventListener('keydown', (e) => {
-//     if (e.key === 'ArrowLeft') {
-//       setInterval(() => {
-//         mySnake.style.left = parseInt(mySnake.style.left) - position + 'px';
-//         if (mySnake.style.left === '-200px') {
-//           mySnake.style.left = parseInt(mySnake.style.left) + position + 'px';
-//           console.log('left limit');
-//         }
-//       }, 100);
-//     }
-//   });
-// };
-// snakeLeft();
-// let snakeRight = () => {
-//   document.addEventListener('keydown', (e) => {
-//     if (e.key === 'ArrowLeft') {
-//       setInterval(() => {
-//         mySnake.style.left = parseInt(mySnake.style.left) + position + 'px';
-//         if (mySnake.style.left === '200px') {
-//           mySnake.style.left = parseInt(mySnake.style.left) - position + 'px';
-//           console.log('right limit');
-//         }
-//       }, 100);
-//     }
-//   });
-// };
-// snakeRight();
-class Snake {
-  constructor() {
-    head: body = 0;
+document.addEventListener('keydown', (e) => {
+  if (interval) {
+    clearInterval(interval);
   }
-}
+  switch (e.key) {
+    case 'ArrowLeft':
+      interval = setInterval(() => {
+        mySnake.style.left = parseInt(mySnake.style.left) - position + 'px';
+        if (mySnake.style.left === '-200px') {
+          mySnake.style.left = parseInt(mySnake.style.left) + position + 'px';
+          console.log('left limit');
+        }
+      }, 100);
+      break;
+
+    case 'ArrowRight':
+      interval = setInterval(() => {
+        mySnake.style.left = parseInt(mySnake.style.left) + position + 'px';
+        if (mySnake.style.left === '200px') {
+          mySnake.style.left = parseInt(mySnake.style.left) - position + 'px';
+          console.log('right limit');
+        }
+      }, 100);
+      break;
+    case 'ArrowUp':
+      interval = setInterval(() => {
+        mySnake.style.top = parseInt(mySnake.style.top) - position + 'px';
+        if (mySnake.style.top === '-200px') {
+          mySnake.style.top = parseInt(mySnake.style.top) + position + 'px';
+          console.log('up limit');
+        }
+      }, 100);
+      break;
+    case 'ArrowDown':
+      interval = setInterval(() => {
+        mySnake.style.top = parseInt(mySnake.style.top) + position + 'px';
+        if (mySnake.style.top === '200px') {
+          mySnake.style.top = parseInt(mySnake.style.top) - position + 'px';
+          console.log('down limit');
+        }
+      }, 100);
+      break;
+  }
+});
+console.log(dot.position.left);
+console.log(dot.position.top);
+// Make the snake a stack, for every dot it eats the snake's body will increase its stack.
